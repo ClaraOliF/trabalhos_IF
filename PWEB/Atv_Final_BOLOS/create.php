57560e5142a3f0edbb07c81e899e100e14ebcd2e
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-05-30 21:01:49 -0300

    Update create.php

--- PWEB/Atv_Final_BOLOS/create.php
+++ PWEB/Atv_Final_BOLOS/create.php
@@ -10,12 +10,36 @@
 
     <?php
         require_once "conexao.php";  
-        echo "chegou aqui";   
     ?>
 
+    <form action="' . $_SERVER['PHP_SELF'] . '" method="post" name="cadastro">
+        <!--inserir imagem-->
+        <label for="produto">
+            Produto: <input type="text" name = "produto">
+        </label>
+        <label for="preco">
+            Preço: <input type="text" name = "preco">
+        </label>
+        <label for="fatias">
+            Fatias: <input type="number" name = "fatias">
+        </label>
+        <label for="data">
+            Data: <input type="date" name = "data">
+        </label>
+        <label for="descricao">
+            Descricão: <input type="textfield" name = "produto">
+        </label>
+        <label for="categorias">
+            Escolha a categoria: <input type="radio" name="categorias">
+        </label>
+
+        <input type="submit" value="Criar">
+        <a href="index.php"><button>Cancelar</button></a>
+        
+    </form>
+
     <!--
-    Criar  formulário
-    Opção de canacelar
+    validar entradas de input
     encerrar conexão
     -->
 </body>
