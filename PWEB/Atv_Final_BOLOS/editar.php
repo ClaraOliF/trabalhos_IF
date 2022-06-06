a1d2c7fab8052298eabe47cd0a95454434e809d5
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-06 17:00:08 -0300

    Add files via upload

---
+++ PWEB/Atv_Final_BOLOS/editar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Editando Produto</title>
+</head>
+<body>
+
+    <?php 
+        echo "cheogu aqui";
+        if(isset( $_GET['id'])){
+            $id = $_GET['id'];
+            echo $id;
+        }  
+    ?>
+    
+</body>
+</html>
