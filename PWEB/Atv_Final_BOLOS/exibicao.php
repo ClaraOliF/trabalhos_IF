5d1d4bb82c96e6c6732908a27801e3fd4b967fd3
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-06 17:02:50 -0300

    Update exibicao.php

--- PWEB/Atv_Final_BOLOS/exibicao.php
+++ PWEB/Atv_Final_BOLOS/exibicao.php
@@ -6,21 +6,43 @@
 <body>
     <?php
     require_once "conexao.php";
+    require_once "funcoes.php";
     $sql = "SELECT * FROM bolos";
     $resultado =  mysqli_query($conn, $sql);
     if(!empty($resultado)){
-        while($row = mysqli_fetch_assoc($resultado)) 
-        foreach($row as $chave => $valor){
-            if($chave == $row['imagem']){
-                echo "<tr><th>".$chave."</th><th><img src=". $row['imagem']."></th></tr>";
-           }else {
-                echo "<tr><th>".$chave."</th><th>".$valor."</th></tr>";
-           }
-           // Adicionando botões de DELETAR e EDITAR
-           //<a href="<?php require 'funcoes.php';deletar();?>" ><button>Deletar</button></a>
-           //<a href="<?php require 'funcoes.php';editar();?>" ><button>Editar</button></a>
-        }
-        
+        while($row = mysqli_fetch_assoc($resultado)) {
+            foreach($row as $chave => $valor){
+                if($chave == $row['imagem']){
+                    echo "<tr>
+                            <th>".$chave."</th>
+                            <th><img src='imagens/". $row['imagem']."'/></th>
+                        </tr>";
+                } elseif($chave=='ID'){
+                    $linha_id = $valor;
+                    echo "<tr>
+                            <th><strong>".$chave."</strong></th>
+                            <th>".$valor."</th>
+                        </tr>";
+                }
+                else {
+                    echo "<tr>
+                            <th><strong>".$chave."</strong></th>
+                            <th>".$valor."</th>
+                        </tr>";
+                   }
+            }
+            // Adicionando botões de DELETAR e EDITAR
+            //echo "<tr>
+                    //<th>
+                       // <a href='deletar.php?id=".$linha_id."'>
+                        //q<button>Deletar</button>
+                   // </th>";
+            //echo   "<th>
+                       // <a href=header("Location:editar.php?id='.$linha_id.")>
+                        //<button>Editar</button></a>
+                    //</th>
+                //</tr>";
+        } 
     } else {
         echo "Ainda não existe nenhum produto cadastrado";
     }  
