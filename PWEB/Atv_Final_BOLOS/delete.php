6d3c753a4a5a50a3ebbaef4e035e911fd92906b2
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-12 01:48:23 -0300

    Update delete.php

--- PWEB/Atv_Final_BOLOS/delete.php
+++ PWEB/Atv_Final_BOLOS/delete.php
@@ -1,1 +1,13 @@
-chegamos aqui
+<?php 
+    if(isset( $_POST['id'])){
+        require_once 'funcoes.php';
+        require 'conexao.php';
+        $id = $_POST['id'];
+        $produto_info = select_by_id($id);
+        if(!empty($produto_info)){
+            $sql = "DELETE FROM bolos where ID='{$id}'";
+            $row = mysqli_query($conn, $sql);
+            header('Location:index.php');
+            }
+   }
+?>
