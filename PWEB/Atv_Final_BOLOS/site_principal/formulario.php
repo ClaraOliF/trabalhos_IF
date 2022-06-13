08002a282c00172d176343b582d2c81ca9dde3a5
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-13 11:50:34 -0300

    Update formulario.php

--- PWEB/Atv_Final_BOLOS/site_principal/formulario.php
+++ PWEB/Atv_Final_BOLOS/site_principal/formulario.php
@@ -5,8 +5,9 @@
     <title>Enviar mensagem</title>
 </head>
 <body>
-    <h1>Enviado mensagem</h1>
+    <h1>Enviando Pedido</h1>
 
+    <!-- 
     <?php
     //require_once "funcoes.php";
     //error_reporting(E_ERROR | E_PARSE);
@@ -15,75 +16,96 @@
             $id = $_POST['id'];
             echo $id;}
     ?>
+    $img_prod = array();
+    $img_prod = push(selecionar_img_prod($id));
+    Exibir imagem selecionada e nome do produto
+    echo "Pedido do ".$img_prod[0].">";
+    echo "<img href=".$img_prod.[1].">";
+    e talvez outras informações tbm
+    -->
 
 <form action="" method="post" name="envio_msg" enctype="multipart/form-data">
+    <!-- Informações de contato obrigatórias-->
+    <label for="nome" class="inserir">
+        *Seu nome: <input type="text" name="nome"><br>
+    </label>
+    <label for="numero" class="inserir ">
+        *Nº de contato: <input type="tel" name="numero"><br>
+    </label>
+    <label for="email" class="inserir">
+        *Seu melhor email: <input type="email" name="email" multiple><br>
+    </label>
+    <label for="entrega"class="inserir" >
+        *Data de entrega: <input type="date" name="entrega"><br>
+    </label>
 
-    <!-- Exibir imagem selecionada e nome do produto-->
-    <!-- data que o pedido foi feito e a data de entrega-->
-    <!-- dados de contato: nome do cliente, números(telefone e whatsapp) e email-->
-    <!-- PERGUNTAS: do que gostou? o qu emudar? ual o tema? o que no topo? obs geral.-->
+    <!-- Perguntas sobre customização do bolo opcionais-->
+    <label for="gostou" class="inserir">
+        Do que você gostou e deseja manter no bolo? 
+        <input type="text" name="gostou">
+    </label>
+    <label for="mudar" class="inserir">
+        O que deseja mudar no bolo? 
+        <input type="text" name="mudar">
+    </label>
+    <label for="tema" class="inserir">
+        Qual o tema que o bolo deverá ter? 
+        <input type="text" name="tema">
+    </label>
+    <label for="topo" class="inserir">
+        O que deve haver no topo do bolo? 
+        <input type="text" name="topo">
+    </label>
+    <label for="obs_geral" class="inserir">
+        Outras observações: 
+        <input type="text" name="obs_geral">
+    </label>
+    
     <!-- Enviar imagem referencia-->
-    <!-- NEMN TODOS OS CAMPOS SÃO OBRIGATÓRIOS-->
     <label for="pic" class="inserir">
-        Enviar imagem: <input type="file" name="pic" accept="image/*">
-    </label>   
-    <label for="produto" class="inserir">
-        Produto: <input type="text" name="produto">
-    </label>
-    <label for="descricao" class="inserir ">
-        Descricão: <input type="textfield" name="descricao">
-    </label>
-    <label for="preco" class="inserir">
-        Preço: <input type="number" step=0.01 min=0 name="preco">
-    </label>
-    <label for="fatias"class="inserir" >
-        Fatias: <input type="number" min=0 name="fatias">
-    </label>
-    <label for="tempo" class="inserir">
-        Tempo mínimo de preparo(Em dias): <input type="number" min=0 name="tempo">
-    </label>
+        Imagem referência: <input type="file" name="pic" accept="image/*">
+    </label> 
 
-    <input type="hidden" name='' value=''>
+    <input type="hidden" name='' value='VALOR $ID PHP AQUI'>
 
     <input type="submit" value="Adicionar"> 
 </form>
     <a href="index.php"><button>Cancelar</button></a>
 
-    <?php 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $produto = trim($_POST['produto']);
-        $descricao = trim($_POST['descricao']);
-        $preco = trim($_POST['preco']);
-        $fatias = trim($_POST['fatias']);
-        $tempo = trim($_POST['tempo']);
-        $inclusao = date("m.d.y"); 
-        $categoria = $_POST['categoria'];
-        $img = $_FILES['pic'];
+    <!--Validação do dados e envio por funcao_enviar()
+    Ao se enviar é definido na funçãoo valor 
+    $data =  data e hora de envio do pedido-->
 
-        // Validação de dados
-        if(!empty($produto) && !empty($descricao) && !empty($preco) && !empty($fatias) 
-        && !empty($tempo) && !empty($inclusao) && !empty($categoria) && !empty($_FILES['pic'])) {
+<?php
+    if(isset($_POST['id'])){ 
+        // Se os campos obrigatórios estiverem todos preenchidos      
+        if(!empty($_POST['nome']) && !empty($_POST['numero']) && !empty($_POST['email']) && !empty($_POST['entrega'])){
 
-            if ($_FILES['pic']["size"] <= 20000000) { // Verificand o tamanho do arquivo 
-                $ext = strtolower(pathinfo($_FILES['pic']['name'],PATHINFO_EXTENSION));
-                if($ext == "jpg" || $ext == "png" || $ext == "jpeg" || $ext == "webp"){
-                    $new_name = date("Y-m-d_H-i-s").".".$ext; //Definindo um novo nome para o arquivo
-                    $dir = './imagens/'; //Diretório para uploads 
-                    move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
-                    $img = $new_name;
-                    // Enviando dados para cadastro
-                    cadastro($img, $produto, $descricao, $preco, $fatias, $tempo, $categoria, $inclusao);
-                    header('Location:index.php');
-                } else{
-                    echo "Formato de arquivo inválido!";
-                }
+            // Verificando se a imagem referencia foi enviada
+            if(!empty$_FILES['pic']){            
+                
+                // Verificando tamanho
+                if($_FILES['pic']["size"] <= 20000000 && $_FILES['pic']["size"] != 0){
+                    $ext = strtolower(pathinfo($_FILES['pic']['name'],PATHINFO_EXTENSION));
+
+                    // Verificando formato
+                    if($ext == "jpg" || $ext == "png" || $ext == "jpeg" || $ext == "webp"){
+                        $new_name = date("Y-m-d_H-i-s").".".$ext; //Definindo um novo nome para o arquivo
+                        $dir = './imagens/'; //Diretório para uploads
+                        move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo                   
+                        $img = $new_name;
+                        // ENVIAR DADOS COM IMAGEM NOVA PRESENTE E VALIDADA
+                        enviar($img, $produto, $descricao, $preco, $fatias, $tempo, $categoria,$id);
+                        header("Location:index.php?");
+                    } else{echo 'Favor enviar formato de imagem válido(jpeg,png ou webp).'; }
+                }  else {echo 'Tamanho inválido.';}  
             } else {
-                echo "Arquivo de Imagem grande demais.";
+                // ENVIAR DADOS SEM IMAGEM NOVA 
+                update($img, $produto, $descricao, $preco, $fatias, $tempo, $categoria,$id);
+                header("Location:index.php?");
             }
-        } else {
-            echo "Preencha todos os campos para que o produto possa ser criado!";
-        } 
-    }
-    ?>   
+        } else {echo 'Preencha todos os campos obrigatórios para um melhor atendimento.'}
+    } else {echo 'id do produto escolhido não identificado.';}       
+?>
 </body>
 </html>
