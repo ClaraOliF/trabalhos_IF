e13c5317177ee098f5c1b58abffc34ef93beac85
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-13 21:03:55 -0300

    Update exibicao.php

--- PWEB/Atv_Final_BOLOS/site_principal/exibicao.php
+++ PWEB/Atv_Final_BOLOS/site_principal/exibicao.php
@@ -2,23 +2,24 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="estilo.css" type="text/css">
 </head>
 <body>
     <?php
-    // Exibindo produtos
     require_once "conexao.php";
     require_once "funcoes.php";
     $sql = "SELECT * FROM bolos";
     $resultado =  mysqli_query($conn, $sql);
     if(!empty($resultado)){
         while($row = mysqli_fetch_assoc($resultado)) {
-            foreach($row as $chave => $valor){
+            foreach($row as $chave => $valor){ // erro está aqui
                 if($chave == $row['imagem']){
-                    ?>  <tr>
-                            <th><?php echo $chave ?></th>
-                            <th><img src='"<?php echo $row['imagem'];?>"'></th>
-                        </tr>
-                    <?php
+                   echo '<div>
+                        <tr>
+                            <td>'.$chave.'></td>
+                            <td><img src="imagens/'.$valor.'"></td>
+                        </tr>'; 
+                    
                 } elseif($chave=='ID'){
                     $linha_id = $valor;
                     echo "<tr>
@@ -31,13 +32,20 @@
                             <th><strong>".$chave."</strong></th>
                             <th>".$valor."</th>
                         </tr>";
+
                 }
             }            
-
-        // Adicionando botão de DELETAR 
-        echo "<tr><th><form action='formulario.php' method='post'>
-                <button type='submit' name='id' value=".$linha_id.">Enviar</button>
+            // '<tr><td><img class="img-thumbnail img-responsive" src='.$foto.' alt="Bolo de Chocolate" width="540" height="360"></td>';
+        // http://127.0.0.1/bolo_pweb/index.php?
+        //Adicionando botão de DELETAR 
+        echo "<tr><th><form action='delete.php' method='post'>
+                <button type='submit' name='id' value=".$linha_id.">Deletar</button>
                 </form><th>";
+        // Adicionando botão de EDITAR
+        echo "<th><form action='editar.php' method='post'>
+                <button type='submit' name='id' value=".$linha_id.">Editar</button>
+                </form><th></tr>
+                </div>" ;
                     
         } 
     }else {
