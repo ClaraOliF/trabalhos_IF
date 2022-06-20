bf8ba165590977375605d69ab6e95f159956aae2
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-20 19:05:55 -0300

    Update index.php

--- PWEB/Atv_Final_BOLOS/crud_bolos/index.php
+++ PWEB/Atv_Final_BOLOS/crud_bolos/index.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Site de confeitaria</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+
 </head>
 <body>
-    <!--<p>Depois adicionar retorno de mensagem da última ação realizada</p>-->
-    <table>
     <?php 
         include "exibicao.php";
     ?>
-    </table>
 
     <a href="create.php">
         <input type="button" value="+">
