442063fda5e98bf5221f2dd3aeb6e1a709af1e4e
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-20 19:04:51 -0300

    Update exibicao.php

--- PWEB/Atv_Final_BOLOS/crud_bolos/exibicao.php
+++ PWEB/Atv_Final_BOLOS/crud_bolos/exibicao.php
@@ -6,51 +6,48 @@
 </head>
 <body>
     <?php
-    require_once "conexao.php";
-    require_once "funcoes.php";
+    require_once "../conexao.php";
+    require_once "../funcoes.php";
     $sql = "SELECT * FROM bolos";
     $resultado =  mysqli_query($conn, $sql);
     if(!empty($resultado)){
         while($row = mysqli_fetch_assoc($resultado)) {
             foreach($row as $chave => $valor){
-                if($chave == $row['imagem']){
-                   echo "<div class='catalogo'>
+                if($valor == $row['imagem']){
+                    echo "<table class='catalogo'>
                         <tr>
-                            <th><?php echo $chave ?></th>
-                            <th><img src='/imagens/".$valor."'></th>
+                            <td><?php echo $chave ?></td>
+                            <td><img src='../imagens/".$valor."'></td>
                         </tr>";
                     
                 } elseif($chave=='ID'){
                     $linha_id = $valor;
                     echo "<tr>
-                            <th><strong>". $chave."</strong></th>
-                            <th>".$valor."</th>
+                            <td><strong>". $chave."</strong></td>
+                            <td>".$valor."</td>
                         </tr>";
                 }
                 else {
                     echo "<tr>
-                            <th><strong>".$chave."</strong></th>
-                            <th>".$valor."</th>
+                            <td><strong>".$chave."</strong></td>
+                            <td>".$valor."</td>
                         </tr>";
-
                 }
             }            
 
         // Adicionando botão de DELETAR 
         echo "<tr><th><form action='delete.php' method='post'>
                 <button type='submit' name='id' value=".$linha_id.">Deletar</button>
-                </form><th>";
+                </form><th><br>";
         // Adicionando botão de EDITAR
         echo "<th><form action='editar.php' method='post'>
                 <button type='submit' name='id' value=".$linha_id.">Editar</button>
-                </form><th></tr>
-                </div>" ;
+                </form><th></tr></table>";
                     
         } 
     }else {
         echo "Ainda não existe nenhum produto cadastrado";
     }  
-
     mysqli_close($conn);
     ?>
 </body>
