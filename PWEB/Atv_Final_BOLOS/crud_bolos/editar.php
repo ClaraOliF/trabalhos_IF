885a34b09021c199960356dc303f16bf9935d311
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-20 19:03:34 -0300

    Update editar.php

--- PWEB/Atv_Final_BOLOS/crud_bolos/editar.php
+++ PWEB/Atv_Final_BOLOS/crud_bolos/editar.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Editando Produto</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
     <?php 
         if(isset($_POST['id'])){
             $id = $_POST['id'];
 
-        require_once "funcoes.php";
+        require_once "../funcoes.php";
 
         $produto_info = select_by_id($id);
         
@@ -17,41 +18,51 @@
         <p>Apenas os espaços preenchidos serão atualizados!</p>";
         }
     ?>
-
-        <form action="edit_valid.php" method="post" name="cadastro_produto" enctype="multipart/form-data">
-        <label for="pic" class="inserir">
-            Enviar nova imagem: <input type="file" name="pic" accept="image/*">
+        <!--Definir exibição de valores já selecionados no cadastro durante a edição -> valor DEFAULT-->
+        <form action='edit_valid.php' method='post' name='cadastro_produto' enctype='multipart/form-data'>
+        <label for='pic' class='inserir' >
+            Enviar nova imagem: <input type='file' name='pic' accept='image/*'><br><img src='../imagens/<?php echo $produto_info[0]?>'><br>
         </label>  
 
         <label for='produto' class='inserir'>
-            Novo nome do produto: <input type='text' name='produto'>
-        </label> ";
+            Novo nome do produto: <input type='text' name='produto' placeholder='<?php echo $produto_info[1];?>'><br>
+        </label>
         
-        <label for="descricao" class="inserir ">
-            Nova descricão: <input type="textfield" name="descricao">
+        <label for='descricao' class='inserir'> 
+            Nova descricão:<br><input type="textarea" style="width:250px;height:150px;" name="descricao" placeholder=
+            '<?php 
+                
+                echo $produto_info[2];
+            ?>'><br>
         </label>
-        <label for="preco" class="inserir">
-            Novo preço: <input type="number" step=0.01 min=0 name="preco">
+        <label for='preco' class='inserir'>
+            Novo preço: <input type='number' step=0.01 min=0 name='preco' placeholder='<?php echo $produto_info[3]." R$";?>'><br>
         </label>
-        <label for="fatias"class="inserir" >
-            Mudar nº Fatias: <input type="number" min=0 name="fatias">
+        <label for='fatias' class='inserir' >
+            Mudar nº Fatias: <input type="number" min=0 name='fatias' placeholder='<?php echo $produto_info[4];?>'><br>
         </label>
-        <label for="tempo" class="inserir">
-            Tempo mínimo de preparo(Em dias): <input type="number" min=0 name="tempo">
+        <label for='tempo' class='inserir'>
+            Tempo médio de preparo(Em dias): <input type='number' min=0 name='tempo' placeholder='<?php echo $produto_info[5]." Dia(s)";?>'><br>
         </label>
 
     <?php
-    //Inserindo categorias
-        require_once 'funcoes.php';
+
+    //Inserindo categorias com a opção marcada
+        require_once '../funcoes.php';
         $resposta = take_tags();
         while ($row = mysqli_fetch_assoc($resposta)) {
             $categoria = $row['nome'];
-            echo "<input type='radio' name='categoria' value=$categoria>
-            <label for='categoria'>$categoria</label><br>";
+            echo "  <label for='fatias' class='inserir' >
+                        <input type='radio' name='categoria' value=".$categoria;
+            if($categoria==$produto_info[7]){
+                echo " checked='checked'><label for='categoria'>$categoria</label><br>";
+            } else {
+                echo "><label for='categoria'>$categoria</label><br>";
             }
+        }        
         echo "<input type='hidden' name='id' value=".$id."></input>";
-    ?>
-        <input type="submit" value="Adicionar">
+    ?><br>
+        <input type="submit" value="Adicionar"> 
     </form>
         <a href="index.php"><button>Cancelar</button></a>    
 </body>
