9d3712de610fd05fb4598ac1f12bcd6f724af400
Clara Oliveira <80792083+ClaraOliF@users.noreply.github.com>
2022-06-20 19:11:15 -0300

    Update exibicao.php

--- PWEB/Atv_Final_BOLOS/site_principal/exibicao.php
+++ PWEB/Atv_Final_BOLOS/site_principal/exibicao.php
@@ -1,57 +1,41 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="estilo.css" type="text/css">
+    <meta charset="UTF-8">    
+    <link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
     <?php
-    require_once "conexao.php";
-    require_once "funcoes.php";
+    // Exibindo produtos
+    require "../conexao.php";
+    require "../funcoes.php";
     $sql = "SELECT * FROM bolos";
     $resultado =  mysqli_query($conn, $sql);
     if(!empty($resultado)){
         while($row = mysqli_fetch_assoc($resultado)) {
-            foreach($row as $chave => $valor){ // erro está aqui
+            foreach($row as $chave => $valor){
                 if($valor == $row['imagem']){
-                   echo '<div>
+                    echo "<div class='catalogo'>
                         <tr>
-                            <td>'.$chave.'></td>
-                            <td><img src="imagens/'.$valor.'"></td>
-                        </tr>'; 
-                    
+                            <th><a href='formulario.php?id=".$row["ID"]."'> 
+                            <img src='../imagens/".$valor."'></th></a><br>
+                        </tr>";
                 } elseif($chave=='ID'){
                     $linha_id = $valor;
                     echo "<tr>
                             <th><strong>". $chave."</strong></th>
-                            <th>".$valor."</th>
+                            <th>".$valor."</th><br>
                         </tr>";
                 }
                 else {
                     echo "<tr>
                             <th><strong>".$chave."</strong></th>
-                            <th>".$valor."</th>
-                        </tr>";
-
+                            <th>".$valor."</th><br>
+                        </tr></div>";
                 }
-            }            
-            // '<tr><td><img class="img-thumbnail img-responsive" src='.$foto.' alt="Bolo de Chocolate" width="540" height="360"></td>';
-        // http://127.0.0.1/bolo_pweb/index.php?
-        //Adicionando botão de DELETAR 
-        echo "<tr><th><form action='delete.php' method='post'>
-                <button type='submit' name='id' value=".$linha_id.">Deletar</button>
-                </form><th>";
-        // Adicionando botão de EDITAR
-        echo "<th><form action='editar.php' method='post'>
-                <button type='submit' name='id' value=".$linha_id.">Editar</button>
-                </form><th></tr>
-                </div>" ;
-                    
+            }        
         } 
-    }else {
-        echo "Ainda não existe nenhum produto cadastrado";
-    }  
-
+    } 
     mysqli_close($conn);
     ?>
 </body>
